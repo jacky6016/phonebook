46e4245da23eed3d4cd3b8740e53dbed79c2bb10
jacky6016 <chlin6016@gmail.com>
2016-09-27 17:58:39 +0800

    Make 'entry' struct more compact for cache performance

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -3,12 +3,10 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
-/* TODO: After modifying the original version, uncomment the following
- * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
+#define OPT 1
+
+typedef struct __ENTRY_DETAIL {
+	char firstName[16];
     char email[16];
     char phone[10];
     char cell[10];
@@ -17,8 +15,14 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} entry_detail;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+	entry_detail *detail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
+
 
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
